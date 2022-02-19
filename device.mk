6d67d972aa73d1d0623ddcce14bda8c5173c9719
TeraaBytee <terabyte3766@gmail.com>
2022-02-19 22:21:14 +0700

    Revert "begonia: Don't inherit updatable_apex.mk"
    This reverts commit 5e9c43bd678f2277b2922eb7690ac9fe585830ec.

--- device.mk
+++ device.mk
@@ -6,6 +6,9 @@
 
 DEVICE_PATH := device/xiaomi/begonia
 
+# Enable updating of APEXes
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
 # MTK Vendor Repos
 MTK_PLATFORM_FOLDER := mt6785
 $(call inherit-product, vendor/mediatek/ims/mtk-ims.mk)
